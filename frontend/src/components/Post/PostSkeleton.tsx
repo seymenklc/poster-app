0f186ba9be242021154cb51ca9225f27bfaa9924
seymennklc <seymennklc@gmail.com>
2023-01-14 17:58:41 +0300

    chore: fix skeleton

--- frontend/src/components/Post/PostSkeleton.tsx
+++ frontend/src/components/Post/PostSkeleton.tsx
@@ -17,9 +17,9 @@ export default function PostSkeleton({ count }: Props) {
                     className="bg-gray-200 w-full h-32 p-3
                     rounded-md flex gap-3 flex-col animate-pulse"
                 >
-                    <div className="w-[200px] bg-gray-300 h-5 rounded-sm" />
-                    <div className="w-[300px] bg-gray-300 h-5 rounded-sm" />
-                    <div className="w-[350px] bg-gray-300 h-5 rounded-sm" />
+                    <div className="w-[100px] sm:w-[180px] bg-gray-300 h-5 rounded-sm" />
+                    <div className="w-[150px] sm:w-[200px] bg-gray-300 h-5 rounded-sm" />
+                    <div className="w-[180px] sm:w-[250px] bg-gray-300 h-5 rounded-sm" />
                 </div>
             ))}
         </>
